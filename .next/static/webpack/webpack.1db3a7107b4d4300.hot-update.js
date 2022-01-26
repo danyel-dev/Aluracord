ece9f0d6a0128c9bb5c5c6c1db1fd134468c40ac
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-01-26 14:03:52 -0300

    page _app.js criada

--- .next/static/webpack/webpack.1db3a7107b4d4300.hot-update.js
+++ .next/static/webpack/webpack.1db3a7107b4d4300.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3288defe7dc0f6fc"; }
+/******/ 	__webpack_require__.h = function() { return "494739f14d99d8ce"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
