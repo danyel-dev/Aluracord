452851a1db610e67fdbce069dc74ab67ad375652
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-01-27 10:12:50 -0300

    template base da pagina chat adicionado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "485ff375f9042f57"; }
+/******/ 		__webpack_require__.h = function() { return "a42756041b3782cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
